6b736305985dbc4f5f4b7a97df27943d2d01f92c
reynaldo-ivan <mlivan@live.com>
2018-03-20 11:24:27 -0600

    Add Api Cluster

--- src/main/java/mx/com/beo/mongo/util/Constantes.java
+++ src/main/java/mx/com/beo/mongo/util/Constantes.java
@@ -19,8 +19,10 @@ public class Constantes {
 	}
 
 	//Constantes para validacion de Headers de los servicios
-	public static final String  HOST_MONGO = "HOST_MONGO"; 
+	public static final String  HOST_MONGO = "HOST_MONGO";
+	public static final String  HOST_MONGO1 = "HOST_MONGO1";
 	public static final String  PUERTO_MONGO = "PUERTO_MONGO"; 
+	public static final String  PUERTO_MONGO1 = "PUERTO_MONGO1"; 
 	public static final String  BASEDATOS_MONGO = "BASEDATOS_MONGO"; 
 	public static final String  USER_MONGO = "USER_MONGO"; 
 	public static final String  CONTRASENIA_USER_MONGO = "PASSWORD_USER_MONGO"; 
@@ -28,5 +30,6 @@ public class Constantes {
 	public static final String  ID = "id"; 
 	public static final String  ID_ID = "_id"; 
 	public static final String BASEDATOS=Urls.BASEDATOS_MONGO.getPath();
+	public static final String NUMERO_PODS = "NUMERO_PODS";
 	
 }
